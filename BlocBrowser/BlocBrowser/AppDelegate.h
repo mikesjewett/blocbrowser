9015b3843354e7ff5bb47b0aaa8b033e5c170bac
Mike Jewett <mikesjewett@gmail.com>
2015-07-02 18:50:27 -0400

    Completed Building BlocBrowser

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Mike Jewett on 6/26/15.
+//  Copyright (c) 2015 Mike Jewett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Mike Jewett on 6/26/15.
+//  Copyright (c) 2015 Mike Jewett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
